63910cc7db40fbd438d20aa2557bee21ef18429c
Marcos Davi <adamsdavi77@gmail.com>
2026-07-09 19:01:50 -0300

    useState, input and useReducer

--- src/components/HookUseState.jsx
+++ src/components/HookUseState.jsx
@@ -11,6 +11,15 @@ const HookUseState = () => {
     setName("Marcos Davi");
   };
 
+  // 2 - useState e input
+  const [age, setAge] = useState(18);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(age);
+  };
+
   return (
     <div>
       {/* 1 - useState */}
@@ -18,6 +27,17 @@ const HookUseState = () => {
       <p>variável: {userName}</p>
       <p>useState: {name}</p>
       <button onClick={ChangeName}> Mudar nome</button>
+      {/* 2 - useState e input */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+      <p>Você tem {age} anos.</p>
+      <hr />
     </div>
   );
 };
