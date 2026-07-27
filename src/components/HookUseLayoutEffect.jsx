8831229d3ca6c36bd178ed31deef6a9738f11ea3
Marcos Davi <adamsdavi77@gmail.com>
2026-07-27 15:22:12 -0300

    useLayoutEffect

---
+++ src/components/HookUseLayoutEffect.jsx
@@ -0,0 +1,28 @@
+import { useLayoutEffect, useEffect, useState } from "react";
+
+const HookUseLayoutEffect = () => {
+  const [name, setName] = useState("Algum nome");
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("2");
+      setName("Mudou de novo!");
+    }, 0);
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log("1");
+    setName("Um Outro nome");
+  }, []);
+
+  console.log(name);
+
+  return (
+    <div>
+      <h2>useLayoutEffect</h2>
+      <p>Nome: {name}</p>
+    </div>
+  );
+};
+
+export default HookUseLayoutEffect;
